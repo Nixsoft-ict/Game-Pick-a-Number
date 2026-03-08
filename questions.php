df395bd5f8d2aa8f26002969c173ced60c6f59a9
Azeez Muyideen <gbolagademuideen1@gmail.com>
2026-03-08 11:44:46 +0100

    Html, css and php code written

---
+++ questions.php
@@ -0,0 +1,14 @@
+<?php
+
+$questions = [
+    "What is the capital of France?",
+    "What is 2 + 2?",
+    "Who wrote 'To Kill a Mockingbird'?",
+    "What is the largest planet in our solar system?",
+    "What is the chemical symbol for water?"
+];
+$numbers = [1, 2, 3, 4, 5];
+
+$combine_questions = array_combine($numbers, $questions);
+
+?>
